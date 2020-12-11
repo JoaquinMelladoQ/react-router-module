8eb93bb074546cc8cf7ab3fa713de8fd1654f130
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-11 14:05:51 -0300

    imported prueba component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import Prueba from './Components/Prueba'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       
+        <Route path="/" component={Prueba} />
       </div>
     );
   }
