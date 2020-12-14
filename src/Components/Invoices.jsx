aab35b21c05276250f35ec411aed614905cdcd18
Joaquin Mellado Quevedo <mellado.joaquin@gmail.com>
2020-12-13 21:00:17 -0300

    added a link to root with a text Volver

--- src/Components/Invoices.jsx
+++ src/Components/Invoices.jsx
@@ -3,6 +3,8 @@ import { link } from 'react-router-dom'
 
 export default class Invoices extends Component {
     render() {
-        return 'este es el componente Invoices'
+        return(
+            <div>Este es el componente Invoices! <link to="/" ></link >Volver</div>
+        )
     }
 }
